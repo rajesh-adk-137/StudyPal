15b3680d348ce1a18fa511745a5781920ce4d890
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-04-30 13:59:00 +0545

    frontend setup with tailwind and react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-black text-white">Copilotkit testing</div>
+    </>
+  )
+}
+
+export default App
