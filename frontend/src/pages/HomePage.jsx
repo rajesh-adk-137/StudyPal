2501b923ab0d3abf8e968b7e65caaa6a8389fa85
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-05-02 14:16:25 +0545

    frontend:routing added to app

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { CopilotKit } from "@copilotkit/react-core";
+import "@copilotkit/react-ui/styles.css";
+import { CopilotPopup } from "@copilotkit/react-ui";
+
+const HomePage = () => {
+    return (
+        <>
+            <div className="bg-black text-white">Copilotkit testing</div>
+            <CopilotKit url="http://localhost:5174/api">
+
+                <div style={{ "--copilot-kit-primary-color": "#7D5BA6" }}>
+
+                    {/* <CopilotTextarea/> */}
+
+                    <CopilotPopup
+                        instructions={
+                            "Help the user manage a todo list. If the user provides a high level goal, " +
+                            "break it down into a few specific tasks and add them to the list"
+                        }
+                        defaultOpen={true}
+                        labels={{
+                            title: "Todo List Copilot",
+                            initial: "Hi you! 👋 I can help you manage your documents.",
+                        }}
+                        clickOutsideToClose={false}
+                    />
+
+
+                </div>
+
+            </CopilotKit>
+        </>
+    )
+}
+
+export default HomePage
